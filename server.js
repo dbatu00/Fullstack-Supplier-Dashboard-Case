a39bfe2fac1adbd62cf3a217ca1162464e7cfbdc
Dbatu00 <denizbatu@sabanciuniv.edu>
2023-12-25 01:11:04 +0300

    connection to mongo done

--- server.js
+++ server.js
@@ -1,47 +1,27 @@
-// server.js
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:admin@cluster0.9f7cuu3.mongodb.net/?retryWrites=true&w=majority"; //productionda env var lazım admin:admin yerine
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+const uri = "mongodb+srv://admin:admin@cluster0.9f7cuu3.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+//const exercisesRouter = require('./routes/exercises');
+//const usersRouter = require('./routes/users');
+//
+//app.use('/exercises', exercisesRouter);
+//app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
 });
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-// Define MongoDB Schemas and Models for orders, parentProducts, and vendors
-
-// Define API routes to retrieve data
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-
-
 
