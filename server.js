4d5fc374393baa84a3bc5fd7f56f38c2137728e8
Dbatu00 <denizbatu@sabanciuniv.edu>
2023-12-25 20:32:06 +0300

    query works

--- server.js
+++ server.js
@@ -8,18 +8,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://admin:admin@cluster0.9f7cuu3.mongodb.net/?retryWrites=true&w=majority"
+const uri = "mongodb+srv://admin:admin@cluster0.9f7cuu3.mongodb.net/LoncaCase?retryWrites=true&w=majority";
+
 mongoose.connect(uri);
 const connection = mongoose.connection;
+
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
+  console.log(connection.name);
 })
 
-//const exercisesRouter = require('./routes/exercises');
-//const usersRouter = require('./routes/users');
-//
-//app.use('/exercises', exercisesRouter);
-//app.use('/users', usersRouter);
+app.get('/api/firstFiveDocuments', async (req, res) => {
+  try {
+    const collection = connection.db.collection('Orders');
+    const documents = await collection.find({}).limit(5).toArray();
+    
+    // Print the first 5 documents to the terminal
+    documents.slice(0, 5).forEach(doc => console.log(doc));
+
+    res.json(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
