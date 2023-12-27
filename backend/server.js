d722f9b71df18bdf1d8756328144dda6f6abb6b7
Dbatu00 <denizbatu@sabanciuniv.edu>
2023-12-27 22:05:51 +0300

    END OF DAY

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,57 @@ async function startServer() {
     const ordersCollection = mongoose.connection.db.collection('Orders');
     const vendorsCollection = mongoose.connection.db.collection('Vendors');
     const parentProductsCollection = mongoose.connection.db.collection('parent_products');
+    const vendorSalesCollection = mongoose.connection.db.collection('vendor_sales');
+
+
+    const insertVendorSales = async (vendorName) => {
+      try {
+        console.log(`Processing vendor: ${vendorName}`);
+        
+        const vendor = await vendorsCollection.findOne({ name: vendorName });
+        if (!vendor) {
+          console.log(`Vendor not found: ${vendorName}`);
+          return;
+        }
+        const vendorId = vendor._id;
+    
+        const products = await parentProductsCollection.find({ 'vendor': vendorId }).toArray();
+        if (products.length === 0) {
+          console.log(`No products found for vendor: ${vendorName}`);
+          return;
+        }
+    
+        const ordersOfProducts = [];
+        for (const product of products) {
+          const orderOfProduct = await ordersCollection.findOne({ 'cart_item.product': product._id });
+    
+          if (orderOfProduct) {
+            ordersOfProducts.push(orderOfProduct);
+            console.log("Order:", orderOfProduct);
+          }
+        }
+        if (ordersOfProducts.length === 0) {
+          console.log(`No orders found for any product for vendor: ${vendorName}`);
+          return;
+        }
+    
+        const vendorSalesDocument = {
+          vendorId: vendorId,
+          orders: ordersOfProducts
+        };
+        const result = await vendorSalesCollection.insertOne(vendorSalesDocument);
+        console.log('Inserted into vendor_sales collection:', result);
+      } catch (error) {
+        console.error(`Error processing vendor ${vendorName}:`, error);
+      }
+    };
+    
+    // Iterate through all vendors and call the function
+    const allVendors = await vendorsCollection.find({}).toArray();
+    for (const vendor of allVendors) {
+      await insertVendorSales(vendor.name);
+    }
+    
 
     //table
     app.get('/api/totalSales', async (req, res) => {
@@ -53,19 +104,27 @@ async function startServer() {
 
       //listeyi struct yapıp her bir product'ın kaç sattığını bul
       //0'dan fazla satan productları, satış sayıları ile gönder
-      const ordersWithProducts = [];
+      const ordersOfProducts = [];
       for (const product of products) {
-        const orderIdWithProduct = await ordersCollection.findOne({ 'cart_item.product': product._id });
+        const orderOfProduct = await ordersCollection.findOne({ 'cart_item.product': product._id });
 
-        if (orderIdWithProduct) {
-          ordersWithProducts.push(orderIdWithProduct);
-          console.log("Order ID:", orderIdWithProduct);
+        if (orderOfProduct) {
+          ordersOfProducts.push(orderOfProduct);
+          console.log("Order ID:", orderOfProduct);
         }
       }
-      if (ordersWithProducts.length === 0) {return res.status(404).json({ message: 'No orders found for any product' });}
+      if (ordersOfProducts.length === 0) {return res.status(404).json({ message: 'No orders found for any product' });}
 
+      // Insert ordersOfProducts into vendor_sales collection
+      const vendorSalesDocument = {
+        vendorId: vendorId,
+        orders: ordersOfProducts,
+        timestamp: new Date(),
+      };
+      const result = await vendorSalesCollection.insertOne(vendorSalesDocument);
+      console.log('Inserted into vendor_sales collection:', result);
 
-      res.json(ordersWithProducts);
+      res.json("dummy string");
 
     } catch (error) {
       console.error(error);
