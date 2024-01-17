9091d78302b463b2a12a1b09b99575b0b26d08b1
Dbatu00 <denizbatu@sabanciuniv.edu>
2024-01-17 22:59:22 +0300

    full functionality

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,24 @@ import './styles.css';
 function App() {
   const [loading, setLoading] = useState(false);
   const [vendor, setVendor] = useState('');
+  const [apiData, setApiData] = useState(null);
 
-  const handleButtonClick = () => {
-    setLoading(true);
-
-    // Simulate loading for 1 second
-    setTimeout(() => {
+  const handleButtonClick = async (endpoint) => {
+    try {
+      setApiData(null); // Clear apiData before fetching new data
+      setLoading(true);
+      console.log(`Fetching data from /api/${endpoint} with vendor name ${vendor}`);
+      const response = await fetch(`http://localhost:5000/api/${endpoint}?vendor=${vendor}`);
+      console.log('Fetched successfully');
+      console.log('Response:', response);
+      const data = await response.json();
+      console.log('Data:', data);
+      setApiData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
       setLoading(false);
-    }, 1000);
+    }
   };
 
   return (
@@ -24,16 +34,19 @@ function App() {
         className="input"
       />
       <div className="button-container">
-        <button className="button" disabled={loading} onClick={handleButtonClick}>
-          Monthly Sales
-        </button>
-        <button className="button" disabled={loading} onClick={handleButtonClick}>
-          Total Sales
-        </button>
+        <button className='button' onClick={() => handleButtonClick('monthlySales')}>Monthly Sales</button>
+        <button className='button' onClick={() => handleButtonClick('totalSales')}>Total Sales</button>
       </div>
       {loading && (
         <div className="spinner-container">
           <div className="spinner"></div>
+        </div>
+      )}
+
+      {apiData && (
+        <div>
+          <h2>API Data:</h2>
+          <pre>{JSON.stringify(apiData, null, 2)}</pre>
         </div>
       )}
     </div>
